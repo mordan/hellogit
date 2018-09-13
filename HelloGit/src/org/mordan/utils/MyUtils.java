b5dff1e2e8d1dbad9280fbc6bcd96a8f7e930511
Charles Bentley <mordan9@gmail.com>
2018-09-13 14:12:31 +0200

    link hellogits

---
+++ HelloGit/src/org/mordan/utils/MyUtils.java
@@ -0,0 +1,12 @@
+package org.mordan.utils;
+
+import hello_gitters.HelloGitters;
+
+public class MyUtils {
+
+	
+	public  static void testHello() {
+		
+		HelloGitters hg = new HelloGitters();
+	}
+}
